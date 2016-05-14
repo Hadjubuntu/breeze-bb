b093bb4c3177156a9113e0f6edf6f5b7ba8c834c
AdrienHS <adrien.hadj.salah@gmail.com>
2016-05-14 12:41:29 +0200

    pwm test

--- scripts/pwm-setup.sh
+++ scripts/pwm-setup.sh
@@ -12,7 +12,7 @@
 config-pin overlay cape-universaln
 
 PIN=$1 # eg. 0
-PIN_NAME=$2 # eg. 9.42
+PIN_NAME=$2 # eg. 9.14
 PERIOD=$3 # eg. 20000000 means 50Hz
 DUTY_CYCLE=$4 # eg. 1500000 ns
 SUBPATH=0
