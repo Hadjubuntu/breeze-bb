e474c945cd7f7d7f923351cac868eb952fa8dd20
AdrienHS <adrien.hadj.salah@gmail.com>
2016-05-22 07:36:35 +0200

    fix gyro

--- src/peripherals/IMU/provider/GyroMPU6050.cpp
+++ src/peripherals/IMU/provider/GyroMPU6050.cpp
@@ -60,11 +60,11 @@ Vect3D GyroMPU6050::read()
 	short buffer[6];
 	int16_t result[3];
 
-	i2c.readFrom(0x3B,6,buffer);
+	i2c.readFrom(0x43, 6, buffer);
 
-	result[0]=(((int16_t)buffer[8]) << 8 ) | buffer[9];
-	result[1]=(((int16_t)buffer[10]) << 8 ) | buffer[11];
-	result[2]=(((int16_t)buffer[12]) << 8 ) | buffer[13];
+	result[0]=(((int16_t)buffer[0]) << 8 ) | buffer[1];
+	result[1]=(((int16_t)buffer[2]) << 8 ) | buffer[3];
+	result[2]=(((int16_t)buffer[4]) << 8 ) | buffer[5];
 
 	result[0] *= 2000.0/32768.0;
 	result[1] *= 2000.0/32768.0;
