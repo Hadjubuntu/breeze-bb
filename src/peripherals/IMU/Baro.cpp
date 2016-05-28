5bf5914e63e8b4bde15c64e15247b56093ffd9a0
AdrienHS <adrien.hadj.salah@gmail.com>
2016-05-28 11:20:21 +0200

    fix

--- src/peripherals/IMU/Baro.cpp
+++ src/peripherals/IMU/Baro.cpp
@@ -194,7 +194,7 @@ void Baro::calculateAltitude()
 		GroundPressure = (long) (alpha*GroundPressure + (1.0-alpha) * truePressure);
 		GroundTemp =  (long) (alpha*GroundTemp  + (1.0-alpha) * trueTemperature);
 
-		printf("Adjusting ground pressure : %.1f | true pressure = %.1f\n", GroundPressure, truePressure);
+		printf("Adjusting ground pressure : %lu | true pressure = %lu \n", GroundPressure, truePressure);
 
 		_altitudeMeters = 0.0;
 	}
