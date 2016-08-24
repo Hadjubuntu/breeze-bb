8b8a13bb12a485d830907fd65b56b628c0fa1a92
AdrienHS <adrien.hadj.salah@gmail.com>
2016-08-24 22:58:21 +0200

    setup

--- scripts/bb-setup.sh
+++ scripts/bb-setup.sh
@@ -41,13 +41,13 @@ do
 		chown ${user} *
 
 		PERIOD=${pwm_freq_list[$idx]} # eg. 20000000 means 50Hz TODO operation division and *10^9
-		DUTY_CYCLE="800000" # min duty cycle =  800 us | eg. 1500000 ns
+		DUTY_CYCLE=800000 # min duty cycle =  800 us | eg. 1500000 ns
 
 		echo "Duty cycle="
 		echo ${DUTY_CYCLE}
 
 		echo ${PERIOD} > period_ns
-		echo ${DUTY_CYCLE} > duty_ns
+		echo 800000 > duty_ns
 		echo 1 > run
 
 		idx=$((idx+1))
