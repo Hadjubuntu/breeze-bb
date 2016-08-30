acc48cf22f7b0aaeff976ef8c80a4b3175d2355b
AdrienHS <adrien.hadj.salah@gmail.com>
2016-08-30 22:01:17 +0200

    sbus correction

--- src/hal/Pwm.cpp
+++ src/hal/Pwm.cpp
@@ -41,6 +41,7 @@ void Pwm::init()
 
 void Pwm::write(long dutyCycleMs)
 {
+	printf("pwm write=%ul\n", dutyCycleMs);
 	FileTools::writeToFile(pwmDutyFilepath, std::to_string(dutyCycleMs * Constants::MS_TO_NS));
 }
 
