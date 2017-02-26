82109a9e719e7137352db507287f3360de609314
AdrienHS <adrien.hadj.salah@gmail.com>
2017-02-26 18:59:59 +0100

    modif conf

--- main.cpp
+++ main.cpp
@@ -148,7 +148,7 @@ void loop()
 				FastMath::toDegrees(rpyTarget[0]), FastMath::toDegrees(rpyTarget[1]), FastMath::toDegrees(rpyTarget[2]),
 				flightControl.getThrottleOut());
 
-		printf("stab0:%.1f | stab1:%.1f | stab2:%.1f | X1=%.2f\n",
+		printf("stab0:%.1f | stab1:%.1f | stab2:%.1f | X1=%d\n",
 				flightStabilization.getTau().getX(),
 				flightStabilization.getTau().getY(),
 				flightStabilization.getTau().getZ(),
