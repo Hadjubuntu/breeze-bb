a6abbf333da5b6dd88f8ca35ff8f254af944bc26
AdrienHS <adrien.hadj.salah@gmail.com>
2017-07-30 11:47:45 +0200

    test gpio python script

--- scripts/rasp/setup-gpio.py
+++ scripts/rasp/setup-gpio.py
@@ -72,10 +72,10 @@ initGPIO(firmware)
 
 
 while time.time() - appStart < 300:
-	pwm.ChangeDutyCycle(7.5)  # turn towards 90 degree
+	pwmObj[0].ChangeDutyCycle(7.5)  # turn towards 90 degree
 	time.sleep(1) # sleep 1 second
-	pwm.ChangeDutyCycle(2.5)  # turn towards 0 degree
+	pwmObj[0].ChangeDutyCycle(2.5)  # turn towards 0 degree
 	time.sleep(1) # sleep 1 second
-	pwm.ChangeDutyCycle(12.5) # turn towards 180 degree
+	pwmObj[0].ChangeDutyCycle(12.5) # turn towards 180 degree
 	time.sleep(1) # sleep 1 second 
 	
