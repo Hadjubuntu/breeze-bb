3fae2f211a2b6c5a50808a29f19813a14d4cf6f7
AdrienHS <adrien.hadj.salah@gmail.com>
2017-08-06 14:11:07 +0200

    test gpio

--- scripts/rasp/gpio/rpiPWM1Test.cpp
+++ scripts/rasp/gpio/rpiPWM1Test.cpp
@@ -14,14 +14,14 @@ unsigned int  setAngle(rpiPWM1 *el, unsigned int degrees){
 
 int main (void){
 
-	rpiPWM1 pwm(50.0, 3600, 7.5, rpiPWM1::MSMODE);
+	rpiPWM1 pwm(50.0, 3600, 2.5, rpiPWM1::MSMODE);
 
 	// 20ms = 3600 counts (Period)
 	// 2ms = 360 counts (10% duty cycle)  => angle 180
 	// 1ms = 180 counts (5% duty cycle) => angle 0
 	// 1.5ms = 180+90 = 270 (7.5%  duty cycle ) => angle = 90 //servo centered
 
-	for (int i=0; i < 270; i +=15)
+	for (int i=0; i < 180; i +=15)
 	{
 		setAngle(&pwm, i);
 		printf("Current angle: %d\n", i);
