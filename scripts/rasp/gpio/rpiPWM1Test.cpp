76fbbca447790d47332b03b12aa94146c584c44d
AdrienHS <adrien.hadj.salah@gmail.com>
2017-08-06 14:10:34 +0200

    test gpio

--- scripts/rasp/gpio/rpiPWM1Test.cpp
+++ scripts/rasp/gpio/rpiPWM1Test.cpp
@@ -21,7 +21,7 @@ int main (void){
 	// 1ms = 180 counts (5% duty cycle) => angle 0
 	// 1.5ms = 180+90 = 270 (7.5%  duty cycle ) => angle = 90 //servo centered
 
-	for (int i=0; i < 180; i +=5)
+	for (int i=0; i < 270; i +=15)
 	{
 		setAngle(&pwm, i);
 		printf("Current angle: %d\n", i);
