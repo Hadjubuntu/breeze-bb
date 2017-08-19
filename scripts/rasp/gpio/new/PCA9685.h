a797c9356934fabfc90b4f209abfa2eabe4d1507
AdrienHS <adrien.hadj.salah@gmail.com>
2017-08-19 16:11:52 +0200

    test gpio new code

---
+++ scripts/rasp/gpio/new/PCA9685.h
@@ -0,0 +1,84 @@
+/*
+ * PCA.h
+ *
+ *  Created on: Aug 19, 2017
+ *      Author: adrien
+ */
+
+#ifndef SCRIPTS_RASP_GPIO_NEW_PCA9685_H_
+#define SCRIPTS_RASP_GPIO_NEW_PCA9685_H_
+
+
+#ifndef PCA9685_HPP
+#define PCA9685_HPP
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include "I2Cdev.h"
+
+#define PCA9685_DEFAULT_ADDRESS     0x40 // All address pins low, Navio default
+
+#define PCA9685_RA_MODE1            0x00
+#define PCA9685_RA_MODE2            0x01
+#define PCA9685_RA_LED0_ON_L        0x06
+#define PCA9685_RA_LED0_ON_H        0x07
+#define PCA9685_RA_LED0_OFF_L       0x08
+#define PCA9685_RA_LED0_OFF_H       0x09
+#define PCA9685_RA_ALL_LED_ON_L     0xFA
+#define PCA9685_RA_ALL_LED_ON_H     0xFB
+#define PCA9685_RA_ALL_LED_OFF_L    0xFC
+#define PCA9685_RA_ALL_LED_OFF_H    0xFD
+#define PCA9685_RA_PRE_SCALE        0xFE
+
+#define PCA9685_MODE1_RESTART_BIT   7
+#define PCA9685_MODE1_EXTCLK_BIT    6
+#define PCA9685_MODE1_AI_BIT        5
+#define PCA9685_MODE1_SLEEP_BIT     4
+#define PCA9685_MODE1_SUB1_BIT      3
+#define PCA9685_MODE1_SUB2_BIT      2
+#define PCA9685_MODE1_SUB3_BIT      1
+#define PCA9685_MODE1_ALLCALL_BIT   0
+
+#define PCA9685_MODE2_INVRT_BIT     4
+#define PCA9685_MODE2_OCH_BIT       3
+#define PCA9685_MODE2_OUTDRV_BIT    2
+#define PCA9685_MODE2_OUTNE1_BIT    1
+#define PCA9685_MODE2_OUTNE0_BIT    0
+
+class PCA9685 {
+    public:
+        PCA9685(uint8_t address = PCA9685_DEFAULT_ADDRESS);
+
+        void initialize();
+        bool testConnection();
+
+        float getFrequency();
+        void setFrequency(float frequency);
+
+        void sleep();
+        void restart();
+
+        void setPWM(uint8_t channel, uint16_t offset, uint16_t length);
+        void setPWM(uint8_t channel, uint16_t length);
+        void setPWMmS(uint8_t channel, float length_mS);
+        void setPWMuS(uint8_t channel, float length_uS);
+
+        void setAllPWM(uint16_t offset, uint16_t length);
+        void setAllPWM(uint16_t length);
+        void setAllPWMmS(float length_mS);
+        void setAllPWMuS(float length_uS);
+
+     private:
+        uint8_t devAddr;
+        float frequency;
+};
+
+#endif // PCA9685_ HPP
+
+
+#endif /* SCRIPTS_RASP_GPIO_NEW_PCA9685_H_ */
---
+++ scripts/rasp/gpio/new/servotest.cpp
@@ -0,0 +1,50 @@
+/*
+ * servotest.cpp
+ *
+ *  Created on: Aug 19, 2017
+ *      Author: adrien
+ */
+
+
+
+
+
+#define NAVIO_RCOUTPUT_1 3
+#define SERVO_MIN 1.250 /*mS*/
+#define SERVO_MAX 1.750 /*mS*/
+
+#include "gpio.h"
+#include "PCA9685.h"
+
+
+using namespace Navio;
+
+int main()
+{
+    static const uint8_t outputEnablePin = RPI_GPIO_27;
+
+
+    Pin pin(outputEnablePin);
+
+    if (pin.init()) {
+        pin.setMode(Pin::GpioModeOutput);
+        pin.write(0); /* drive Output Enable low */
+    } else {
+        fprintf(stderr, "Output Enable not set. Are you root?\n");
+        return 1;
+    }
+
+    PCA9685 pwm;
+
+    pwm.initialize();
+    pwm.setFrequency(50);
+
+    while (true) {
+        pwm.setPWMmS(NAVIO_RCOUTPUT_1, SERVO_MIN);
+        sleep(1);
+        pwm.setPWMmS(NAVIO_RCOUTPUT_1, SERVO_MAX);
+        sleep(1);
+    }
+
+    return 0;
+}
