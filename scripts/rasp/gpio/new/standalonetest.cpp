edd9a11e9d445170f0f86327b403d2566b5abdef
AdrienHS <adrien.hadj.salah@gmail.com>
2017-10-08 16:02:38 +0200

    Test pwm

--- scripts/rasp/gpio/new/standalonetest.cpp
+++ scripts/rasp/gpio/new/standalonetest.cpp
@@ -308,8 +308,8 @@ int main(int argc, char **argv)
 	setServo(450);
 	sleep(2);
 
-	printf("Position: 550\n");
-	setServo(550);
+	printf("Position: 500\n");
+	setServo(500);
 	sleep(2);
 
 	close();
